d65648c2b82cc63adbacbcc365d6a261085c6a5b
Taylor Franklin <tlf09a@acu.edu>
2014-08-26 22:18:29 -0500

    Initial commit of Xcode 6 beta 6 project.

---
+++ TiltingLoaderTests/TiltingLoaderTests.swift
@@ -0,0 +1,36 @@
+//
+//  TiltingLoaderTests.swift
+//  TiltingLoaderTests
+//
+//  Created by Taylor Franklin on 8/26/14.
+//  Copyright (c) 2014 Taylor Franklin. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TiltingLoaderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
